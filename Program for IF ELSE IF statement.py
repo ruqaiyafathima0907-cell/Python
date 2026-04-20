955532d9c3c8a9bffafd8a9b14537c7c3ca22817
ruqaiyafathima0907-cell <ruqaiyafathima0907@gmail.com>
2026-04-20 20:50:09 +0530

    Add files via upload

---
+++ Program for IF ELSE IF statement.py
@@ -0,0 +1,10 @@
+marks=input("Enter your marks: ")
+if marks >="90":
+    print("Grade is A")
+elif marks >="70":
+    print("Grade is B")
+elif marks >="50":
+    print("Grade is C")
+else:
+    print("Grade is D")
+
