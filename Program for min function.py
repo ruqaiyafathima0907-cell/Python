f13e551ff7d11fe13c5829c3c72a84e352055635
ruqaiyafathima0907-cell <ruqaiyafathima0907@gmail.com>
2026-05-05 07:57:58 +0530

    Add files via upload

---
+++ Program for min function.py
@@ -0,0 +1,4 @@
+num=[1,2,3,4,5,6]
+lang=['java','c','python','cpp','sql','SQL']
+print("Min of the list: ",min(num))
+print("Min of the list: ",min(lang))
---
+++ Program for sum function.py
@@ -0,0 +1,4 @@
+list1=[1,2,3,4,5,6]
+print("Sum of the list: ",sum(list1))
+list2=[2,4,6,8,10,12]
+print("Sum of both lists: ",sum(list1+list2))
