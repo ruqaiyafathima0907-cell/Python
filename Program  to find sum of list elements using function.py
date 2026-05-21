94052cbb51c5b7796f3586c8f2c08d3f4badf405
ruqaiyafathima0907-cell <ruqaiyafathima0907@gmail.com>
2026-05-21 23:12:31 +0530

    Add files via upload

---
+++ Program  to find sum of list elements using function.py
@@ -0,0 +1,4 @@
+def list_sum(num):
+    return sum(num)
+data=[10,20,30,40]
+print("Sum= ",list_sum(data))
---
+++ Program to check even or odd number using function.py
@@ -0,0 +1,6 @@
+def check_odd_even(num):
+    if num%2==0:
+        print(num,"is even number")
+    else:
+        print(num,"is odd number")
+check_odd_even(15)
